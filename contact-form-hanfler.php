c3a53f419a95b2d7919830a3e9ba181d64322faa
flamur1234 <59200987+flamur1234@users.noreply.github.com>
2022-11-12 12:44:43 +0100

    Update contact-form-hanfler.php

--- contact-form-hanfler.php
+++ contact-form-hanfler.php
@@ -1,1 +1,23 @@
+<?php
 
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$message = $_POST['message'];
+
+$email_from = 'hflamur153@gmail.com';
+$email_subject = "New From Submission";
+$email_body = "User Name:$name.\n".
+                "User Email:$visitor_email.\n".
+                  "User Message:$message.\n";
+
+$to = "hflamur153@gmail.com";
+$headers="From: $email_from \r\n";
+$headers = "Replay-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location:Protofolio.html")
+
+
+
+
+
+?>
